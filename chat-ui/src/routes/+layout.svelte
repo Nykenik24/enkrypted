c94c564f55c88e4daed2421ae188df4a40a8dd08
sogeking <daviddoichita@proton.me>
2026-04-25 23:45:32 +0200

    feat: improve app layout and header style

--- chat-ui/src/routes/+layout.svelte
+++ chat-ui/src/routes/+layout.svelte
@@ -34,23 +34,37 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<div
-	class="w-full bg-neutral-200 dark:bg-neutral-900 p-4 flex justify-between items-center"
->
-	<div>
-		<a href="/"
-			><h2 class="text-xl font-bold hover:cursor-pointer">
-				Enkrypted
-			</h2></a
-		>
+<div class="flex flex-col h-screen">
+	<div
+		class="w-full bg-emerald-500 dark:bg-emerald-800 p-4 flex justify-between items-center shrink-0"
+	>
+		<div>
+			<a href="/"
+				><h2
+					class="text-xl font-bold hover:cursor-pointer font-stretch-150%"
+				>
+					Enkrypted
+				</h2></a
+			>
+		</div>
+		<div class="flex gap-5">
+			{#if connected}
+				<p
+					class="p-2 rounded-md preset-filled-success-700-300 dark:preset-filled-success-100-900"
+				>
+					Status: Connected
+				</p>
+			{:else}
+				<p
+					class="p-2 rounded-md preset-filled-error-700-300 dark:preset-filled-error-100-900"
+				>
+					Status: Disconnected
+				</p>
+			{/if}
+			<a href="/login" class="btn preset-filled">Login</a>
+		</div>
 	</div>
-	{#if connected}
-		<p class="p-2 rounded-md preset-filled-success-400-600">
-			Connected to backend
-		</p>{:else}
-		<p class="p-2 rounded-md preset-filled-error-400-600">
-			Disconnected from backend
-		</p>{/if}
-	<div><a href="/login" class="btn preset-filled">Login</a></div>
+	<main class="flex-1 overflow-y-auto bg-neutral-300 dark:bg-neutral-900">
+		{@render children()}
+	</main>
 </div>
-{@render children()}
