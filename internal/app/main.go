9da6b68758b8cb09e7bd3e3dc181fd1c36d89b89
sogeking <daviddoichita@proton.me>
2026-05-01 00:03:36 +0200

    feat: config and use slog and move startup hook to config

--- internal/app/main.go
+++ internal/app/main.go
@@ -1,43 +1,67 @@
 package app
 
 import (
-	"log"
+	"log/slog"
+	"os"
+	"strings"
 
+	"github.com/Nykenik24/enkrypted/internal/config"
 	"github.com/Nykenik24/enkrypted/internal/db"
 	"github.com/Nykenik24/enkrypted/internal/routes"
 	"github.com/gofiber/fiber/v3"
 )
 
-const VERSION = "v0.0.1"
+const addr = ":8080"
+
+var log *slog.Logger
+
+func initLogger() {
+	levelStr := os.Getenv("LOG_LEVEL")
+	if levelStr == "" {
+		levelStr = "info"
+	}
+
+	var level slog.Level
+	switch strings.ToLower(levelStr) {
+	case "debug":
+		level = slog.LevelDebug
+	case "info":
+		level = slog.LevelInfo
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+
+	// Configure the global default logger
+	slog.SetDefault(slog.New(
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: level,
+		}),
+	))
+}
 
 func Start() {
+	initLogger()
+
 	app := fiber.New()
 
-	app.Hooks().OnPreStartupMessage(func(sm *fiber.PreStartupMessageData) error {
-		sm.BannerHeader = `                     __                                        __                      __
-                    /  |                                      /  |                    /  |
-  ______   _______  $$ |   __   ______   __    __   ______   _$$ |_     ______    ____$$ |
- /      \ /       \ $$ |  /  | /      \ /  |  /  | /      \ / $$   |   /      \  /    $$ |
-/$$$$$$  |$$$$$$$  |$$ |_/$$/ /$$$$$$  |$$ |  $$ |/$$$$$$  |$$$$$$/   /$$$$$$  |/$$$$$$$ |
-$$    $$ |$$ |  $$ |$$   $$<  $$ |  $$/ $$ |  $$ |$$ |  $$ |  $$ | __ $$    $$ |$$ |  $$ |
-$$$$$$$$/ $$ |  $$ |$$$$$$  \ $$ |      $$ \__$$ |$$ |__$$ |  $$ |/  |$$$$$$$$/ $$ \__$$ |
-$$       |$$ |  $$ |$$ | $$  |$$ |      $$    $$ |$$    $$/   $$  $$/ $$       |$$    $$ |
- $$$$$$$/ $$/   $$/ $$/   $$/ $$/        $$$$$$$ |$$$$$$$/     $$$$/   $$$$$$$/  $$$$$$$/
-                                        /  \__$$ |$$ |
-                                        $$    $$/ $$ |
-                                         $$$$$$/  $$/                                     ` + VERSION + "\n------"
-
-		return nil
-	})
+	app.Hooks().OnPreStartupMessage(config.OnPreStartupMessageHook)
 
 	db := db.GetInstance().Database
 	database, err := db.DB()
 	if err != nil {
-		log.Fatalf("could not get db instance: %s", err.Error())
+		slog.Error("could not get db instance", "error", err)
+		panic(1)
 	}
 	defer database.Close()
 
 	routes.RegisterAll(app)
 
-	log.Fatal(app.Listen(":8080"))
+	err = app.Listen(addr)
+	if err != nil {
+		slog.Error("error binding server to address", "address", addr, "error", err)
+	}
 }
