cc9183eaffd44bf57b6260f55b7c840209809040
Nykenik24 <Nykenik24@proton.me>
2026-05-02 00:50:57 +0200

    fix: missing space

--- internal/config/hooks.go
+++ internal/config/hooks.go
@@ -51,7 +51,7 @@ func OnPostStartupMessageHook(sm *fiber.PostStartupMessageData) error {
 	funFacts = append(funFacts, fmt.Sprintf("This message is random, and you just got a 1/%d chance!", len(funFacts)+1))
 
 	fmt.Println()
-	fmt.Println("Welcome to \x1b[34;1mthe enkrypted REPL\x1b[0m! You can manage your\x1b[35;3menkrypted server\x1b[0m with it.")
+	fmt.Println("Welcome to \x1b[34;1mthe enkrypted REPL\x1b[0m! You can manage your \x1b[35;3menkrypted server\x1b[0m with it.")
 	fmt.Printf("\x1b[1mFun fact\x1b[0m: \x1b[90;3m%s\x1b[0m\n", util.Choice(funFacts))
 	fmt.Println()
 	fmt.Println("Write \x1b[32mquit\x1b[0m \x1b[3mor\x1b[0m \x1b[32mq\x1b[0m to \x1b[91mclose the program\x1b[0m")
